3af08c7de0d3d90d85ad3e05254de7fc865f98ff
Nicholas March <n.march213@gmail.com>
2016-10-07 19:56:24 -0500

    yolo

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express")
+
+var app = express();
+
+// Template Engine - Handlebars
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main'
+});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+	res.render("home");
+});
+
+app.get("/about", function(req, res){
+	res.render("about");
+});
+
+app.listen(3000, function(){
+	console.log("YelpCamp Started!");
+});
