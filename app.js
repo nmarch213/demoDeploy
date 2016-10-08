6e9ec0e6b2c1805ebe2de396a28f4f5f7c5805c0
Nicholas March <n.march213@gmail.com>
2016-10-07 20:10:13 -0500

    yolo2

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.get("/about", function(req, res){
 	res.render("about");
 });
 
-app.listen(3000, function(){
-	console.log("YelpCamp Started!");
+app.listen(process.env.PORT, function(){
+	console.log("Demo Started at " + process.env.PORT);
 });
